1824859c9ec61675e8c81b155e291a690af20d0a
Kain <kain.nguyen@terralogic.com>
2025-05-13 17:34:10 +0700

    fix: in between icon path

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -2,7 +2,7 @@ export const icons = {
   google: require('./google-icon.png'),
   apple: require('./apple-icon.png'),
   check: require('./check.png'),
-  inBetween: require('./inBetween.png'),
+  inBetween: require('./in-between.png'),
   down: require('./arrow-down.png'),
   up: require('./arrow-up.png'),
 };
